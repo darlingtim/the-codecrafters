6788c3a3c2d12ad9a5a15d2ee109e1fe63c0547e
Timothy Ododo <timothyododo@gmail.com>
2026-03-31 00:15:39 +0100

    initial scaffold for the command-and-control program

---
+++ command-and-control/baseConvertor/convert.go
@@ -0,0 +1,154 @@
+package baseconvertor
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func binary(b string) int64 {
+	result, err := strconv.ParseInt(b, 2, 64)
+	if err != nil {
+		fmt.Printf("An error occured while converting from binary to decimal:\n %v", err)
+
+	}
+	return result
+}
+
+func hexadecimal(b string) int64 {
+	result, err := strconv.ParseInt(b, 16, 64)
+	if err != nil {
+		fmt.Printf("an error occured while converting from hexadecimal to decimal:\n %v", err)
+
+	}
+	return result
+}
+
+func decimal(dec string) string {
+	number, err := strconv.Atoi(dec)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Binary: %b\n", number)
+	fmt.Printf("Hex: %X\n", number)
+	return ""
+}
+
+// main function
+func main() {
+
+	fmt.Print("Welcome to CodeCrafters NumberBase Converter..\n This Converter can only help you convert binary and hexadecimal to decimal and can also give you the binary and hexadecimal value of any decimal number of your choice .\n")
+	exampleOperations := []string{
+		"\n",
+		"Here are the examples of all operations you can perform.\n",
+		"\n",
+		"convert 1E hex - Decimal: 30\n",
+		"convert 10 bin - Decimal: 2\n",
+		"convert 255 dec - Binary:  11111111  Hex: FF\n",
+		"help - will show you all you need to effectively use this converter\n",
+		"quit - will stop and exit the converter program\n",
+	}
+
+	fmt.Print("\n", exampleOperations, "\n", "\n")
+
+enterArgument:
+	fmt.Println("Please enter an operation")
+
+	userInput := bufio.NewScanner(os.Stdin)
+
+	if userInput.Scan() {
+		arg := strings.Fields(userInput.Text())
+		if userInput.Text() == "" || strings.TrimSpace(userInput.Text()) == "" {
+			fmt.Println("no input was gotten from you.")
+			goto enterArgument
+
+		}
+		if len(arg) == 1 {
+			if userInput.Text() == "quit" {
+				fmt.Print("\n", "Thanks for using codeCrafters Base Converter\n")
+				return
+			} else if userInput.Text() == "help" {
+				//helpFile, err3 := os.ReadFile("README.md")
+				/*if err3 != nil {
+					fmt.Println("An error occured in fetching help file, try again or carry out another operation")
+					goto enterArgument
+				}*/
+				fmt.Print("\n", exampleOperations, "\n")
+				goto enterArgument
+
+			} else if userInput.Text() == "convert" || userInput.Text() == "Convert" || userInput.Text() == "CONVERT" {
+				fmt.Printf("%q should be followed by a number that you want to convert then the binary name. Type help to see examples of how the commands should be entered.\n", userInput.Text())
+				goto enterArgument
+
+			} else {
+				fmt.Printf("%v is not a valid operation. First Arguement must be convert, help or quit\n", userInput.Text())
+				goto enterArgument
+			}
+		} else if len(arg) == 2 || len(arg) > 3 {
+			fmt.Println("invalid number of inputs recievied. enter the right format or press help to see the needed input format")
+			goto enterArgument
+		} else {
+			if arg[0] == "convert" || arg[0] == "Convert" || arg[0] == "CONVERT" {
+				switch arg[2] {
+				case "bin":
+					for _, s := range arg[1] {
+						if strings.ContainsAny(string(s), "23456789") || unicode.IsLetter(s) {
+
+							fmt.Printf("\nThe number you are trying to convert to decimal is not a valid binary number. should only contain %q qnd %q\n", "0", "1")
+							goto enterArgument
+						}
+					}
+					fmt.Println(binary(arg[1]))
+					goto enterArgument
+				case "hex":
+					check := true
+					for _, s := range arg[1] {
+						if strings.ContainsAny(string(s), "abcdefABCDEF") || unicode.IsDigit(s) {
+
+						} else {
+							check = false
+							break
+						}
+					}
+					if check == true {
+						fmt.Println(hexadecimal(arg[1]))
+						goto enterArgument
+					} else {
+						fmt.Printf("\nThe number you are trying to convert to decimal is not a valid HexaDecimal number. should only contain digits and letters from %q to %q\n", "A", "F")
+						goto enterArgument
+					}
+
+				case "dec":
+					check := true
+					for _, number := range arg[1] {
+						if unicode.IsLetter(number) {
+							check = false
+							break
+						}
+					}
+					if check == true {
+						fmt.Println(decimal(arg[1]))
+						goto enterArgument
+					} else {
+						fmt.Printf("\nThe number you are trying to convert, is not a valid Decimal number. It should only contain digits from %q to %q\n", "0", "9")
+						goto enterArgument
+					}
+
+					//decimal(arg[1])
+				default:
+					fmt.Printf("converting %v is not currently supported\n", arg[2])
+					goto enterArgument
+
+				}
+			} else {
+				fmt.Println("unrecognized conversion format entered.")
+				goto enterArgument
+			}
+		}
+
+	}
+
+}
---
+++ command-and-control/calculator/cal.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//fmt.Println(os.Args)
+	fmt.Print("Welcome to CodeCrafters Calculator..\n This calculator can only help you to add, subtract, multiply and divide any two numbers of your choice.\n")
+	exampleOperations := []string{
+		"\n",
+		"Here are the examples of all operations you can perform.\n",
+		"\n",
+		"add 2 5 - add 2 and 5 to give you 7\n",
+		"sub 5 3 - subtract two 5 and 3 to give you 2\n",
+		"mul 7 4 - multiplies 7 and 4 to give you 28\n",
+		"div 8 2 - divides 8 with 2 to give you 4\n",
+		"help - will show you all you need to effectively use this calculator\n",
+		"quit - will stop and exit the calculator program\n",
+	}
+
+	fmt.Print("\n", exampleOperations, "\n", "\n")
+
+enterArgument:
+	fmt.Println("Please enter an operation")
+	argument := bufio.NewScanner(os.Stdin)
+
+	if argument.Scan() {
+		arg := strings.Fields(argument.Text())
+
+		if len(arg) == 1 {
+			if argument.Text() == "quit" {
+				fmt.Print("\n", "Thanks for using codeCrafters calculator\n")
+				return
+			} else if argument.Text() == "help" {
+				//helpFile, err3 := os.ReadFile("README.md")
+				/*if err3 != nil {
+					fmt.Println("An error occured in fetching help file, try again or carry out another operation")
+					goto enterArgument
+				}*/
+				fmt.Print("\n", exampleOperations, "\n")
+				goto enterArgument
+
+			} else if argument.Text() == "add" {
+				fmt.Print("\n", "you did not provide the two numbers that you want to add.", "\n")
+				goto enterArgument
+
+			} else if argument.Text() == "sub" {
+				fmt.Print("\n", "you did not provide the two numbers that you want to subtract.", "\n")
+				goto enterArgument
+
+			} else if argument.Text() == "mul" {
+				fmt.Print("\n", "you did not provide the two numbers that you want to multiply.", "\n")
+				goto enterArgument
+
+			} else if argument.Text() == "div" {
+				fmt.Print("\n", "you did not provide the two numbers that you want to divide.", "\n")
+				goto enterArgument
+
+			} else {
+				fmt.Print("\n", "you entered a wrong operation.", "\n")
+				goto enterArgument
+
+			}
+		}
+
+		if len(arg) == 2 || len(arg) > 3 {
+			fmt.Print("\n", "incorrect number of argument. E.g. add 3 5", "\n")
+			goto enterArgument
+
+		} else if len(arg) == 3 {
+
+			funcArg1, err := strconv.Atoi(arg[1])
+			funcArg2, err1 := strconv.Atoi(arg[2])
+
+			if err != nil || err1 != nil {
+				fmt.Print("\n", "An error occured, Try again with the right parameters", "\n")
+
+			}
+
+			if strings.ContainsAny(arg[1], "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") || strings.ContainsAny(arg[2], "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+				fmt.Println("Function arguments must be a number")
+				goto enterArgument
+			}
+
+			if arg[0] == "add" {
+				result := fmt.Sprint(add(funcArg1, funcArg2))
+				fmt.Print("\n", result, "\n", "\n")
+				goto enterArgument
+			}
+
+			if arg[0] == "sub" {
+				result := fmt.Sprint(sub(funcArg1, funcArg2))
+				fmt.Print("\n", result, "\n", "\n")
+				goto enterArgument
+			}
+
+			if arg[0] == "mul" {
+				result := fmt.Sprint(mul(funcArg1, funcArg2))
+				fmt.Print("\n", result, "\n", "\n")
+				goto enterArgument
+			}
+
+			if arg[0] == "div" {
+				if funcArg2 == 0 {
+					fmt.Println(" Second number must be either 1 or greater")
+					goto enterArgument
+				} else {
+					result := fmt.Sprint(div(funcArg1, funcArg2))
+					fmt.Print("\n", result, "\n", "\n")
+					goto enterArgument
+				}
+			}
+			fmt.Print("\n", "The math operation you provided is either invalid or not supported at the moment", "\n")
+			goto enterArgument
+		}
+
+	} else {
+		return
+	}
+
+}
+
+func add(i int, j int) int {
+	return i + j
+}
+
+func sub(i int, j int) int {
+	return i - j
+}
+
+func mul(i int, j int) int {
+	return i * j
+}
+
+func div(i int, j int) int {
+	return i / j
+}
